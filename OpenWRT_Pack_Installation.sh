13ec7d0dbc06eda3d1f2acc519b1aee56ab3e578
6donote4 <do_note@hotmail.com>
2019-02-07 06:05:48 +0800

    My OpenWRT Translation Scripts

--- OpenWRT_Pack_Installation.sh
+++ OpenWRT_Pack_Installation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-opkg find luci-app-*>>awk '{print $1}' pack.list
-for i in `cat ./pack.list
+opkg find luci-app-* luci-theme-* luci-i18n-*-zh-cn>>pack.list
+awk '{print $1}' pack.list>>pack.list.o
+
+for i in `cat ./pack.list.o
 do
     opkg install $i
 done
+rm ./pack.list ./pack.list.o
 exit 0
