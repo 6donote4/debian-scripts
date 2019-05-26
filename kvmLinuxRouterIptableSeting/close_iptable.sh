735279e5e67a9b49f489c89efb62890d089a42fe
6donote4 <do_note@hotmail.com>
2019-05-26 22:21:01 +0800

    change kvmLinuxRouter

--- kvmLinuxRouterIptableSeting/close_iptable.sh
+++ kvmLinuxRouterIptableSeting/close_iptable.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 IPT="iptables"
 IPTS="iptables-save"
-
+LANIP="192.168.2.0/255.255.255.0"
+WANIP="192.168.1.0/255.255.255.0"
+NETIF0="bridge0" #Inernet interface
+NETIF1="wlp4s0" #Internet interface
 $IPT -F
 $IPT -X
 $IPT -t nat -F
@@ -13,5 +16,5 @@ $IPT -t raw -X
 $IPT -P INPUT ACCEPT
 $IPT -P FORWARD ACCEPT
 $IPT -P OUTPUT ACCEPT
-$IPTS
+$IPT -t nat -A POSTROUTING -o NETIF1 -j MASQUERADE$IPTS
 exit 0
