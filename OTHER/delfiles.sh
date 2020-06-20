0c061634e568542f09f2e47a3ac11c852ffa6806
6donote4 <6donote4@gmail.com>
2020-06-21 05:19:45 +0800

    update delfiles to 0.0.6:

--- OTHER/delfiles.sh
+++ OTHER/delfiles.sh
@@ -4,121 +4,169 @@ export PATH
 #========================================
 #   Linux Distribution: Manjaro/Debian 8+/
 #   Author: 6donote4 <mailto:do_note@hotmail.com>
-#   Dscription: Delete some files in Linux
-#   Version: 0.0.5
+#   Dscription:Delete some files in Linux
+#   Version: 0.0.6
 #   Blog: https://www.donote.tk https://6donote4.github.io
 #========================================
-# This script is used to delete selected files in Linux
-VERSION=0.0.5
+#This script is used to delete selected files in Linux
+VERSION=0.0.6
 PROGNAME="$(basename $0)"
-
 export LC_ALL=C
-
 SCRIPT_UMASK=0122
 umask $SCRIPT_UMASK
-
-usage() {
-cat << EOF
-delfiles $VERSION
-
-Usage:
-./$PROGNAME [option]
-Options
--s  Delete specified files in specified size
--e  Delete empty directory
--n  Delete files of specified name in limited size
--g  Delete fils of the greater than specified size
--r  Delete duplicated files
---version  Show version
--h --help  Show this usage
-EOF
-}
-
+args=($(getopt -o snegtrh -l help,version -- "$@"))
+set -- "$args";
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-
+usage() {
+cat << EOF
+$PROGNAME $VERSION
+Usage:
+./$PROGNAME [option]
+Options
+-s  Delete specified files in specified size
+-n  Delete files of specified name in limited size
+-e  Delete empty directory
+-g  Delete files of the greater than specified size
+-r  Delete duplicated files
+-t  Delete files modification time during specified time
+--version  Show version
+-h --help  Show this usage
+EOF
+}
 if [[ "$1" == ""  ]];then
     usage
     exit 0
 fi
+read_fun() {
+        read -p "$1" RESPON
+        echo $RESPON
+        }
 
-ARGS=( "$@" )
+print_fun() {
+        echo -e "${yellow}You inputed :${plain}"
+        echo $1 >&1
+        echo -e "${green}print done${plain}"
+        }
 
-while [[ -n "$1" ]];
+test_fun() {
+    print_fun ${ARG=$(read_fun "read_print_test:" )}
+    echo -e "${green}test done${plain}"
+}
+
+main(){
+    usage
+    exit 0
+}
+
+
+while [ -n "$1" ]
 do
 	case "$1" in
-           -s)
-                        read -p "Please input file size:" FILESIZE
-                        echo "filesize = $FILESIZE "
-                        echo
-                        read -p "Please input max directory depth:" DEPTH
-                        echo "filedepth = $DEPTH "
-                        echo
-			find . -maxdepth $DEPTH -size -$FILESIZE ! -iname '*.sh' -type f -exec ls {} \;
-                        find . -maxdepth $DEPTH -size -$FILESIZE ! -iname '*.sh' -type f -delete
-	                echo "done"
-	                exit 0
-			;;
-	    -n)
-			read -p "Please input file name:" FILENAME
-			echo "filename = $FILENAME "
-			echo
-                        read -p "Please input file size:" FILESIZE
-                        echo "filesize = $FILESIZE "
-                        echo
-                        read -p "Please input max directory depth:" DEPTH
-                        echo "filedepth = $DEPTH "
-                        echo
-			find . -maxdepth $DEPTH -size -$FILESIZE -iname "$FILENAME" ! -iname '*.sh' -type f -exec ls {} \;
-                        find . -maxdepth $DEPTH -size -$FILESIZE -iname "$FILENAME" ! -iname '*.sh' -type f -delete
-	                echo "done"
-	                exit 0
-			;;
-	    -g)
-			read -p "Please input file name:" FILENAME
-			echo "filename = $FILENAME "
-			echo
-                        read -p "Please input file size:" FILESIZE
-                        echo "filesize = $FILESIZE "
-                        echo
-                        read -p "Please input max directory depth:" DEPTH
-                        echo "filedepth = $DEPTH "
-                        echo
-			find . -maxdepth $DEPTH -size +$FILESIZE -iname "$FILENAME" ! -iname '*.sh' -type f -exec ls {} \;
-                        find . -maxdepth $DEPTH -size +$FILESIZE -iname "$FILENAME" ! -iname '*.sh' -type f -delete
-	                echo "done"
-	                exit 0
-			;;
-	    -e)
-			find . -empty -delete
-			echo "done"
-			exit 0
-			;;
-	    -r)
-			read -p "Please input max directory depth:" DEPTH
-                        echo "filedepth = $DEPTH "
-			echo
-			find . -maxdepth $DEPTH  ! -iname '*.sh' -type f -print0 | xargs -0 md5sum | sort > ./allfiles;
-                        cat ./allfiles | uniq -w 32 > ./uniqfiles;
-			comm ./allfiles ./uniqfiles -2 -3 | cut -c 35- | tr '\n' '\0' | xargs -0 rm;
-			rm ./allfiles ./uniqfiles;
-			echo "done"
-			exit 0
-			;;
-	    -h|--help)
-			usage
-			exit 0
-			;;
-	    --version)
-			echo $VERSION
-			exit 0
-			;;
-
-	    *)
-			echo  "Invalid parameter $1" 1>&2
-			exit 1
-			;;
+        -s)
+            FILESIZEMIN=$(read_fun "Please input file minimum size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZEMIN
+            FILESIZEMAX=$(read_fun "Please input file maximum size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZEMAX
+            DEPTH=$(read_fun "Please input max directory depth:")
+            print_fun $DEPTH
+            find . -maxdepth $DEPTH -size +$FILESIZEMIN -size -$FILESIZEMAX ! -iname '*.sh' -type f -exec ls {} \;
+            if [[ $(read_fun "Do you comfirm to delete files?(y|n):") == "y" || "Y" ]] ;then
+                find . -maxdepth $DEPTH -size +$FILESIZEMIN -size -$FILESIZEMAX ! -iname '*.sh' -type f -delete;
+                echo "done"
+            fi
+            exit 0
+            ;;
+        -n)
+            FILENAME=$(read_fun "Please input file name:")
+            print_fun $FILENAME
+            FILESIZEMIN=$(read_fun "Please input file minimum size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZEMIN
+            FILESIZEMAX=$(read_fun "Please input file maximum size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZEMAX
+            DEPTH=$(read_fun "Please input max directory depth:")
+            print_fun $DEPTH
+            find . -maxdepth $DEPTH -size +$FILESIZEMIN -size -$FILESIZEMAX -iname "$FILENAME" ! -iname '*.sh' -type f -exec ls {} \;
+            if [[ $(read_fun "Do you comfirm to delete files?(y|n):") == "y" || "Y" ]] ;then
+                find . -maxdepth $DEPTH -size +$FILESIZEMIN -size -$FILESIZEMAX -iname "$FILENAME" ! -iname '*.sh' -type f -delete
+                echo "done"
+            fi
+            exit 0
+            ;;
+        -g)
+            FILENAME=$(read_fun "Please input file name:")
+            print_fun $FILENAME
+            FILESIZE=$(read_fun "Please input file size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZE
+            DEPTH=$(read_fun "Please input max directory depth:")
+            print_fun $DEPTH
+            find . -maxdepth $DEPTH -size +$FILESIZE -iname "$FILENAME" ! -iname '*.sh' -type f -exec ls {} \;
+            if [[ $(read_fun "Do you comfirm to delete files?(y|n):") == "y" || "Y" ]] ;then
+                find . -maxdepth $DEPTH -size +$FILESIZE -iname "$FILENAME" ! -iname '*.sh' -type f -delete
+                echo "done"
+            fi
+            exit 0
+            ;;
+        -e)
+            find . -empty -delete
+            echo "done"
+            exit 0
+            ;;
+        -r)
+            DEPTH=$(read_fun "Please input max directory depth:")
+            print_fun $DEPTH
+            find . -maxdepth $DEPTH  ! -iname '*.sh' -type f -print0 | xargs -0 md5sum | sort > ./allfiles;
+            cat ./allfiles | uniq -w 32 > ./uniqfiles
+            cat ./uniqfiles
+            if [[ $(read_fun "Do you comfirm to delete files?(y|n):") == "y" || "Y" ]] ;then
+                comm ./allfiles ./uniqfiles -2 -3 | cut -c 35- | tr '\n' '\0' | xargs -0 rm;
+                echo "done"
+            fi
+            rm ./allfiles ./uniqfiles;
+            echo "done"
+            exit 0
+            ;;
+        -t)
+            FILENAME=$(read_fun "Please input file name:")
+            print_fun $FILENAME
+            FILESIZEMIN=$(read_fun "Please input file minimum size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZEMIN
+            FILESIZEMAX=$(read_fun "Please input file maximum size(b,block;c,bytes;w,two-byte;k,KiB;M,MiB;G,GiB):")
+            print_fun $FILESIZEMAX
+            DEPTH=$(read_fun "Please input max directory depth:")
+            print_fun $DEPTH
+            FTIMESTART=$(read_fun "Please input filetime to modify in begining(198906040101.59):")
+            print_fun $FTIMESTART
+            FTIMEEND=$(read_fun "Please input filetime to modify in end(198906040501.59):")
+            print_fun $FTIMEEND
+            touch -t $FTIMESTART t1.timestamp
+            touch -t $FTIMEEND t2.timestamp
+            find . -maxdepth $DEPTH -size +$FILESIZEMIN -size -$FILESIZEMAX -iname "$FILENAME" -type f \( -newer ./t1.timestamp -a -not -newer ./t2.timestamp \) -exec ls {} \;
+            if [[ $(read_fun "Do you comfirm to delete files?(y|n):") == "y" || "Y" ]] ;then
+                find . -maxdepth $DEPTH -size +$FILESIZEMIN -size -$FILESIZEMAX -iname "$FILENAME" -iname '*.sh' -type f -newer ./t1.timestamp -a -not -newer ./t2.timestamp -delete
+                echo "done"
+            fi
+            rm -rf ./*.timestamp
+            exit 0
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -v|--version)
+            echo $VERSION
+            exit 0
+            ;;
+        --)
+            main
+            exit 0
+            ;;
+        *)
+            echo -e "${red}Invalid parameter $1 ${plain}" 1>&2
+            break
+            ;;
 	esac
+    shift
 done
