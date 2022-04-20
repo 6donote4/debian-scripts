ffca17ea86af875b451f22d234f0ba8863476ad4
6donote4 <6donote4@gmail.com>
2022-04-21 04:43:26 +0800

    add my iptables rules

--- iptables_rules/drop_IP.sh
+++ iptables_rules/drop_IP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #lastb |awk '{print $3}' |sort -n|uniq|sed '2d'|xargs -I[] iptables -A INPUT -p tcp -s [] -j DROP
-cat  messages |awk '{print $11}'|sed 's/SRC=//'|sort -n|uniq|xargs -I[] iptables -A INPUT -A INPUT -p tcp -s [] -j DROP
+cat  /var/log/messages |awk '{print $11}'|sed 's/SRC=//'|sort -n|uniq|xargs -I[] iptables -A INPUT -A INPUT -p tcp -s [] -j DROP
 exit 0
