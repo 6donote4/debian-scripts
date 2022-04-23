8620c77d182b17b4f281719b668784dfba02d4e2
6donote4 <6donote4@gmail.com>
2022-04-23 13:16:23 +0800

    update drop_ip script

--- iptables_rules/drop_IP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#lastb |awk '{print $3}' |sort -n|uniq|sed '2d'|xargs -I[] iptables -A INPUT -p tcp -s [] -j DROP
-cat  /var/log/messages |awk '{print $11}'|sed 's/SRC=//'|sort -n|uniq|sed '/rsyslogd/d'|xargs -I[] iptables -A INPUT -p tcp --dport -s [] -j DROP
-iptables-save
-exit 0
