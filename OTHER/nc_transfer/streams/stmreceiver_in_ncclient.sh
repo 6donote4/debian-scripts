e234249de492520a718d67a274bd6c6f07493dc2
6donote4 <6donote4@gmail.com>
2022-10-19 01:18:33 +0800

    add recever_nc_client/server send_nc_client/server

--- OTHER/nc_transfer/streams/stmreceiver_in_ncclient.sh
+++ OTHER/nc_transfer/streams/stmreceiver_in_ncclient.sh
@@ -4,5 +4,5 @@ echo "This is Client end."
 echo "Client is Receiving a media stream....."
 echo "Please send a media stream from sender_in_ncserver."
 echo "Please input TargetIPaddr,Port."
-nc -nv $1 $2 |mplay -vo x11 -cache 3000 -
+nc -nv $1 $2 |mplayer -vo x11 -cache 3000 -
 exit 0
